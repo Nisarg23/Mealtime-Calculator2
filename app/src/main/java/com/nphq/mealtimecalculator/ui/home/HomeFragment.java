150205f1379fd352c0798bbd0acc39a0c25dcdf3
Sofia Marinou <67066139+sofi8020@users.noreply.github.com>
2020-10-08 18:35:33 -0400

    add view1 on click listener

--- app/src/main/java/com/nphq/mealtimecalculator/ui/home/HomeFragment.java
+++ app/src/main/java/com/nphq/mealtimecalculator/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.nphq.mealtimecalculator.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +9,13 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.nphq.mealtimecalculator.R;
+import com.nphq.mealtimecalculator.homeActivities.FoodActivity;
 
 import org.w3c.dom.Text;
 
@@ -27,7 +30,13 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-
+        CardView view1 = root.findViewById(R.id.view1);
+        view1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), FoodActivity.class));
+            }
+        });
 
 
 
