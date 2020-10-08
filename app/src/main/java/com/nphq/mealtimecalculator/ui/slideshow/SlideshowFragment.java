be3605b9371e41b54eefdcca87a53509edf12aed
Nisarg Patel <npate112@uottawa.ca>
2020-10-08 17:46:55 -0400

    Create contact functionality and UI

--- app/src/main/java/com/nphq/mealtimecalculator/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/nphq/mealtimecalculator/ui/slideshow/SlideshowFragment.java
@@ -1,9 +1,12 @@
 package com.nphq.mealtimecalculator.ui.slideshow;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,14 +18,48 @@ import com.nphq.mealtimecalculator.R;
 public class SlideshowFragment extends Fragment {
 
     private SlideshowViewModel slideshowViewModel;
+    private EditText mEditTextMessage;
+    private EditText mEditTextTo;
+    private EditText mEditTextSubject;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_exercise);
+
+
+        mEditTextMessage = root.findViewById(R.id.edit_text_message);
+        mEditTextTo = root.findViewById(R.id.edit_text_to);
+        mEditTextSubject = root.findViewById(R.id.edit_text_subject);
+
+        Button buttonSend = root.findViewById(R.id.button_send);
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail();
+            }
+        });
+
+
 
         return root;
+    }
+
+    private void sendMail(){
+        String recipient = mEditTextTo.getText().toString();
+        String[] recipients = recipient.split(",");
+
+        String subject = mEditTextSubject.getText().toString();
+        String message = mEditTextMessage.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL,recipients);
+        intent.putExtra(Intent.EXTRA_SUBJECT,subject);
+        intent.putExtra(Intent.EXTRA_TEXT,message);
+
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent, "Choose an email client"));
     }
 }
